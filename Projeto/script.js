c7ba12002e241089d2a8a0928abdb5b7582edfb3
TomasWP <tomasfonseca14@ua.pt>
2025-04-08 22:23:06 +0100

    Update site

---
+++ Projeto/script.js
@@ -0,0 +1,172 @@
+// TODO: Full collision detection
+// TODO: Enemy spawning
+// TODO: End game scenario
+await import('https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js');    
+
+import * as THREE from 'https://unpkg.com/three@0.150.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;          
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+class Box extends THREE.Mesh {
+    constructor({width,height,depth,color = '#00ff00', velocity = {x: 0, y: 0, z: 0}, position={x: 0, y: 0, z: 0}}) {
+        super(
+            new THREE.BoxGeometry(width, height, depth), 
+            new THREE.MeshStandardMaterial({color})
+        );
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+
+        this.position.set(position.x, position.y, position.z);
+
+        this.bottom = this.position.y - (this.height / 2);
+        this.top = this.position.y + (this.height / 2);
+
+        this.velocity = velocity;
+        this.gravity = -0.002;
+    }
+
+    update(ground){
+        this.bottom = this.position.y - this.height / 2;
+        this.top = this.position.y + this.height / 2;
+
+        this.position.x += this.velocity.x;     //x axis movement
+        this.position.z += this.velocity.z;     //z axis movement
+        this.applyGravity();                    //speed of gravity
+    }
+
+    applyGravity() {                     
+        this.velocity.y += this.gravity; //gravity
+
+        if(this.bottom + this.velocity.y <= ground.top) {   //monnitor ground collision
+            this.velocity.y *= 0.8; //bounce friction
+            this.velocity.y = -this.velocity.y;        
+        }else{
+            this.position.y += this.velocity.y;
+        }
+    }
+}
+
+//cube
+const cube = new Box({width: 1, height: 1, depth: 1, velocity: {x: 0, y: -0.01, z: 0}});
+cube.castShadow = true;
+scene.add(cube);
+
+//ground
+const ground = new Box({width: 5, height: 0.5, depth: 10, color: '#ff8c00', position: {x: 0, y: -2, z: 0}});
+ground.receiveShadow = true;
+scene.add(ground);
+
+//Light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.x = 3;
+light.position.y = 4;
+light.position.z = -3;
+light.castShadow = true;
+scene.add(light);
+
+//camera position
+camera.position.z = 8;
+camera.position.x = 5;
+camera.position.y = 2;
+camera.lookAt(cube.position);
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    }
+}
+
+window.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'KeyW': 
+        case 'ArrowUp': 
+            keys.w.pressed = true;
+            break;
+
+        case 'KeyA': 
+        case 'ArrowLeft':
+            keys.a.pressed = true;
+            break;
+
+        case 'KeyD': 
+        case 'ArrowRight':
+            keys.d.pressed = true;
+            break;
+        
+        case 'KeyS':
+        case 'ArrowDown':
+            keys.s.pressed = true;
+            break;
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'KeyW': 
+        case 'ArrowUp': 
+            keys.w.pressed = false;
+            break;
+
+        case 'KeyA': 
+        case 'ArrowLeft':
+            keys.a.pressed = false;
+            break;
+
+        case 'KeyD': 
+        case 'ArrowRight':
+            keys.d.pressed = false;
+            break;
+
+        case 'KeyS':
+        case 'ArrowDown':
+            keys.s.pressed = false;
+            break;
+    }
+})
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+
+  //movement code
+  cube.velocity.x = 0;      //reset velocity
+  cube.velocity.z = 0;      
+  if (keys.a.pressed) {
+    cube.velocity.x -= 0.03;
+  } else if (keys.d.pressed) {
+    cube.velocity.x = 0.03;
+  }
+  
+  if (keys.w.pressed) {
+    cube.velocity.z = -0.035;
+  } else if (keys.s.pressed) {
+    cube.velocity.z = 0.035;
+  }
+
+  cube.update(ground);
+}
+animate();
